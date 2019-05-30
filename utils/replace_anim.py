7200400db7e2a96606689bae382e4b186acb69bf
Nick <NicholasMoser56@gmail.com>
2019-05-29 22:22:59 -0500

    Add script for replacing CSS animations

---
+++ utils/replace_anim.py
@@ -0,0 +1,60 @@
+'''
+Replaces a character select screen animation file with an animation file
+from battle animations. Simply run the script with quickbms.exe, naruto_mot.bms,
+0000.mot, and 0001.mot in the same directory. It will then list all of the
+animation identifiers in 0000.mot that are smaller than the idle animation
+in 0001.mot. Because they are smaller, they can be replaced without issue.
+Simply input the number and it will modify 0001.mot for you.
+'''
+import os
+import subprocess
+import shutil
+import sys
+
+QUICK_BMS = 'quickbms.exe'
+MOT_BMS = 'naruto_mot.bms'
+MOT_0000 = '0000.mot'
+MOT_0001 = '0001.mot'
+FILES_0000 = '0000'
+FILES_0001 = '0001'
+
+# Check that files exist
+if not os.path.isfile(QUICK_BMS):
+    print('{} not found.'.format(QUICK_BMS))
+    sys.exit(1)
+if not os.path.isfile(MOT_BMS):
+    print('{} not found.'.format(MOT_BMS))
+    sys.exit(1)
+if not os.path.isfile(MOT_0000):
+    print('{} not found.'.format(MOT_0000))
+    sys.exit(1)
+if not os.path.isfile(MOT_0001):
+    print('{} not found.'.format(MOT_0001))
+    sys.exit(1)
+
+# Unpack .mot files into .gnta files
+subprocess.run([QUICK_BMS, "-Y", MOT_BMS, MOT_0000, FILES_0000])
+subprocess.run([QUICK_BMS, "-Y", MOT_BMS, MOT_0001, FILES_0001])
+
+# List animations in the .gnta files for 0000.mot
+original_file = os.path.join(FILES_0001, '0.gnta')
+original_size = os.path.getsize(original_file)
+mot_files = os.listdir(FILES_0000)
+for mot_file in mot_files:
+    if os.path.getsize(os.path.join(FILES_0000, mot_file)) <= original_size:
+        # QuickBMS only cannot replace files in archives with larger files than the original
+        print(mot_file.split('.')[0])
+
+# User input animation will replace 0001.mot from 0000.mot
+animation = input('Please select from the above animations: ')
+animation_file = os.path.join(FILES_0000, '{}.gnta'.format(animation))
+if not os.path.isfile(animation_file):
+    print('{} not found.'.format(animation_file))
+    sys.exit(1)
+shutil.copy(animation_file, original_file)
+
+subprocess.run([QUICK_BMS, "-r", "-w", "-Y", "naruto_mot.bms", "0001.mot", "0001"])
+
+shutil.rmtree(FILES_0000)
+shutil.rmtree(FILES_0001)
+print('Done.')
