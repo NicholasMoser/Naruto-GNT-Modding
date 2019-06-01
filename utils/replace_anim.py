d8068581ff0815e14eca10c2df205603f949cbac
Nick <NicholasMoser56@gmail.com>
2019-06-01 13:14:29 -0500

    Refactor replace_anim to handle ALL animations from 0000

--- utils/replace_anim.py
+++ utils/replace_anim.py
@@ -33,27 +33,28 @@ if not os.path.isfile(MOT_0001):
     sys.exit(1)
 
 # Unpack .mot files into .gnta files
-subprocess.run([QUICK_BMS, "-Y", MOT_BMS, MOT_0000, FILES_0000])
-subprocess.run([QUICK_BMS, "-Y", MOT_BMS, MOT_0001, FILES_0001])
+subprocess.run([QUICK_BMS, '-Y', '-Q', MOT_BMS, MOT_0000, FILES_0000])
+subprocess.run([QUICK_BMS, '-Y', '-Q', MOT_BMS, MOT_0001, FILES_0001])
 
 # List animations in the .gnta files for 0000.mot
 original_file = os.path.join(FILES_0001, '0.gnta')
 original_size = os.path.getsize(original_file)
-mot_files = os.listdir(FILES_0000)
-for mot_file in mot_files:
-    if os.path.getsize(os.path.join(FILES_0000, mot_file)) <= original_size:
-        # QuickBMS only cannot replace files in archives with larger files than the original
-        print(mot_file.split('.')[0])
+mot_files = [int(mot_file.split('.')[0]) for mot_file in os.listdir(FILES_0000)]
 
 # User input animation will replace 0001.mot from 0000.mot
-animation = input('Please select from the above animations: ')
+animation = input('\nPlease select a number from 0 to {} inclusive: '.format(max(mot_files)))
 animation_file = os.path.join(FILES_0000, '{}.gnta'.format(animation))
 if not os.path.isfile(animation_file):
     print('{} not found.'.format(animation_file))
     sys.exit(1)
 shutil.copy(animation_file, original_file)
 
-subprocess.run([QUICK_BMS, "-r", "-w", "-Y", "naruto_mot.bms", "0001.mot", "0001"])
+# We must use force in order to repack a large file into the motion archive.
+# This is okay to do since 0001.mot only contains a single animation.
+p = subprocess.run([QUICK_BMS, '-r', '-w', '-Y', '-Q', 'naruto_mot.bms', MOT_0001, FILES_0001],
+                   stdout=subprocess.PIPE,
+                   input='force',
+                   encoding='ascii')
 
 shutil.rmtree(FILES_0000)
 shutil.rmtree(FILES_0001)
