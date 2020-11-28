5f47726cbd43834ac167f81c829d9ae7783e79a5
Nick <NicholasMoser56@gmail.com>
2020-11-27 19:35:19 -0600

    Refactor seq_reader_gnt4 to use function variable

--- utils/seq_reader_gnt4.lua
+++ utils/seq_reader_gnt4.lua
@@ -1,9 +1,9 @@
 file = io.open("C:/Users/kites/Desktop/test.log", "a")
 io.output(file)
 io.write("Begin seq_reader.lua", "\n")
-io.write("File            Offset   Opcode", "\n")
+io.write("File             Offset   Opcode", "\n")
 
-dolphin.hook_instruction(0x800c9138, function ()
+function parse_seq()
     -- The start of the seq file is at *(int *)(seq_p[5] + 0x5c)
     seq_p = dolphin.ppc.gpr[3]
     temp_p = dolphin.mem_u32[seq_p + 0x14]
@@ -19,7 +19,7 @@ dolphin.hook_instruction(0x800c9138, function ()
     -- Get the file name, which can be found at seq_p[8][5]
     file_entry = dolphin.mem_u32[seq_p + 0x20]
     file_name = dolphin.str_read(file_entry + 0x14, 0x10)
-    file_name_str = string.format("%15s ", string.sub(file_name, 0, 0xf))
+    file_name_str = string.format("%-16s ", string.sub(file_name, 0, string.len(file_name) - 1))
 
     -- Get the current opcode being executed
     opcode = dolphin.mem_u32[seq_pc]
@@ -27,4 +27,6 @@ dolphin.hook_instruction(0x800c9138, function ()
 
     -- Append the data to the log file
     io.write(file_name_str, offset_str, opcode_str, "\n")
-end)
+end
+
+dolphin.hook_instruction(0x800c9138, parse_seq)
