4cd51cd2f8473b547b3299d8aadfece8247e1bbd
Nick <NicholasMoser56@gmail.com>
2020-12-13 15:45:04 -0600

    Create script to find symbol map name duplicates

---
+++ utils/find_duplicates_gnt4_map.py
@@ -0,0 +1,16 @@
+# Find duplicate function names in the GNT4 map file.
+# We need to avoid duplicates so that decomp can work.
+
+with open('../general/symbol_maps/G4NJDA.map', 'r') as f:
+    lines = f.readlines()
+print(len(lines))
+a = []
+for count, line in enumerate(lines):
+    if count >= 5335: # Just check musyx.a for now
+        parts = line.split(' ')
+        if len(parts) > 4:
+            name = parts[4]
+            if name in a:
+                print('found: ' + name)
+            if name:
+                a.append(name)
