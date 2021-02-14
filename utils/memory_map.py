666adea5d0752e75477c241216e5e284cbc1447c
Nicholas Moser <NicholasMoser56@gmail.com>
2021-02-13 23:21:09 -0600

    Add script to find unidentified symbol map functions

--- utils/memory_map.py
+++ utils/memory_map.py
@@ -1,30 +1,37 @@
-# Tool for working with memory maps
+'''
+Tool for working with memory maps
+'''
 
 import sys
 
 def main():
-    if (len(sys.argv) < 3):
+    ''' Main function, responsible for parsing args and running the appropriate tool. '''
+    if len(sys.argv) < 3:
         print('Usage:')
         print('-v   Check the memory map for validity.')
         print('-l   Map length check.')
+        print('-z   Find functions that still are unidentified.')
         print('Example:')
         print('python memory_map.py -l RNEEDA.map')
         sys.exit(0)
     arg = sys.argv[1]
-    if (arg == '-v'):
-        is_map_valid(sys.argv[2])
-    elif (arg == '-l'):
-        map_length_check(sys.argv[2])
+    if arg == '-v':
+        is_map_valid()
+    elif arg == '-l':
+        map_length_check()
+    elif arg == '-z':
+        find_unidentified_functions()
     else:
         print('Unknown argument: ' + arg)
         sys.exit(1)
     sys.exit(0)
 
-def is_map_valid(map_file):
+def is_map_valid():
     ''' Checks that the given memory map file is a valid memory map. '''
     # Check that the address in the 1st and 3rd spot of each line match
-    with open('../general/symbol_maps/' + map_file, 'r') as f:
-        lines = f.readlines()
+    map_file = sys.argv[2]
+    with open('../general/symbol_maps/' + map_file, 'r') as open_file:
+        lines = open_file.readlines()
     for count, line in enumerate(lines):
         parts = line.split(' ')
         if line.startswith('8'):
@@ -33,22 +40,39 @@ def is_map_valid(map_file):
             if first != second:
                 print('Not equal on line ' + str(count + 1))
 
-def map_length_check(map_file):
-    with open('../general/symbol_maps/' + map_file, 'r') as f:
-        lines = f.readlines()
+def map_length_check():
+    '''
+    Checks the length of each function in the symbol map and prints which ones may be incorrect.
+    The reason that some lengths are incorrect is because they may contain a branch that shouldn't
+    count for the function length, but did count when Dolphin automatically generated the symbol
+    map. It accomplished this by counting the difference in bytes between the start and end
+    of the function.
+    '''
+    map_file = sys.argv[2]
+    with open('../general/symbol_maps/' + map_file, 'r') as open_file:
+        lines = open_file.readlines()
     previous = []
     for _, line in enumerate(lines):
         parts = line.split(' ')
         if line.startswith('8'):
             addr = int(parts[0], 16)
-            if (previous):
-                previousLength = int(previous[1], 16)
-                previousAddr = int(previous[0], 16)
-                diff = addr - previousAddr
-                if (diff < previousLength):
-                    print('Function: {} ({:02X})'.format(previous[4].strip(), previousAddr))
-                    print('  Expected: {:02X} Actual: {:02X}'.format(previousLength, diff))
+            if previous:
+                previous_length = int(previous[1], 16)
+                previous_addr = int(previous[0], 16)
+                diff = addr - previous_addr
+                if diff < previous_length:
+                    print('Function: {} ({:02X})'.format(previous[4].strip(), previous_addr))
+                    print('  Expected: {:02X} Actual: {:02X}'.format(previous_length, diff))
             previous = parts
+
+def find_unidentified_functions():
+    ''' Print out the functions that have yet to be identified (named). '''
+    map_file = sys.argv[2]
+    with open('../general/symbol_maps/' + map_file, 'r') as open_file:
+        lines = open_file.readlines()
+    for line in lines:
+        if len(line) > 31 and line[29:32] == 'zz_':
+            print('Found ' + line[29:-1])
 
 if __name__ == "__main__":
     main()
